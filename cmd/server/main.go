ddea470a3921a20d3379b17867b07e5b212c1801
Eduardo Alexandre <luis.edu.alex@gmail.com>
2024-05-04 21:15:34 -0300

    chore: add product router and handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/dualexandre/fullcycle-api-golang/configs"
+	"github.com/dualexandre/fullcycle-api-golang/internal/entity"
+	"github.com/dualexandre/fullcycle-api-golang/internal/infra/database"
+	"github.com/dualexandre/fullcycle-api-golang/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
