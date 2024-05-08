fbdcddb5bc0038357065f8d37a6f5d5dc9f06000
Eduardo Alexandre <luis.edu.alex@gmail.com>
2024-05-07 22:19:57 -0300

    chore: add user handler jwt function

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -3,20 +3,59 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/dualexandre/fullcycle-api-golang/internal/dto"
 	"github.com/dualexandre/fullcycle-api-golang/internal/entity"
 	"github.com/dualexandre/fullcycle-api-golang/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB       database.UserInterface
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
 }
 
-func NewUserHandler(userDB database.UserInterface) *UserHandler {
+func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, JwtExpiresIn int) *UserHandler {
 	return &UserHandler{
-		UserDB: userDB,
+		UserDB:       userDB,
+		Jwt:          jwt,
+		JwtExpiresIn: JwtExpiresIn,
 	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
+	})
+
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
